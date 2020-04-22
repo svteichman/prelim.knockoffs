3a7aa2aa384be491343cb37d8ce77616b76c516c
Sarah Teichman <steichman18@amherst.edu>
2020-04-21 21:35:33 -0700

    add prelim.knockoffs.R file

---
+++ R/prelim.knockoffs.R
@@ -0,0 +1,16 @@
+#' Knockoff package documentation (implemented for prelim).
+#'
+#' The knockoff controls the false discovery rate in the Gaussian linear model with fixed-X.
+#'
+#' @name prelim-knockoffs-package
+#' @aliases prelim-knockoffs
+#' @references
+#'   Barber and Candes,
+#'   Controlling the false discovery rate via knockoffs.
+#'   Ann. Statist. 43 (2015), no. 5, 2055--2085.
+#'   \href{https://projecteuclid.org/euclid.aos/1438606853}
+#'
+#' @details
+#' This runs the knockoff filter for a given X matrix and y vector. This controls the false discovery
+#' rate at a given level.
+NULL
