2ccce25536d27ab738c5e4ddfb18311021c0a4b3
Sarah Teichman <steichman18@amherst.edu>
2020-05-05 13:52:21 -0700

    updating BHq base to use svd to get Sigma_inv

--- R/run_BHq_base.R
+++ R/run_BHq_base.R
@@ -17,7 +17,15 @@
 run_BHq_base <- function(X, y, fdr) {
   n <- nrow(X)
   p <- ncol(X)
-  Sigma_inv <- solve(t(X) %*% X )
+
+  X_svd <- svd_sign(X)
+  d <- X_svd$d
+  d_inv = 1 / d
+  v <- X_svd$v
+
+  Sigma_inv <- v %*% diag(d_inv^2) %*% t(v)
+
+  #Sigma_inv <- solve(t(X) %*% X )
   Sig_inv_diag <- diag(Sigma_inv)
   beta_ols <- Sigma_inv %*% t(X) %*% y
   sig_hat <- sqrt(sum((y - X %*% beta_ols)^2)/(n-p)) #estimate of sigma
